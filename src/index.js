cefd2454f06f9e86e4615882954588ca4c6c7427
nico <nico@roombeast.com>
2023-09-13 19:27:26 -0300

    ejercicio 1.11

--- src/index.js
+++ src/index.js
@@ -29,11 +29,9 @@ const Feedback = ({ good, setGood, neutral, setNeutral, bad, setBad }) => {
 
 const StatisticLine = ({ text, value }) => {
   return (
-    <>
-      <p>
-        {text} {value}
-      </p>
-    </>
+    <tr>
+      <td>{text}</td> <td>{value}</td>
+    </tr>
   );
 };
 
@@ -49,18 +47,22 @@ const Statistics = ({ good, neutral, bad }) => {
   return (
     <>
       <h1>statistics</h1>
-      <StatisticLine text="good" value={good} />
-      <StatisticLine text="neutral" value={neutral} />
-      <StatisticLine text="bad" value={bad} />
-      <StatisticLine text="all" value={good + neutral + bad} />
-      <StatisticLine
-        text="average"
-        value={(good - bad) / (good + neutral + bad)}
-      />
-      <StatisticLine
-        text="positive"
-        value={(good / (good + neutral + bad)) * 100}
-      />
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+          <StatisticLine text="all" value={good + neutral + bad} />
+          <StatisticLine
+            text="average"
+            value={(good - bad) / (good + neutral + bad)}
+          />
+          <StatisticLine
+            text="positive"
+            value={(good / (good + neutral + bad)) * 100}
+          />
+        </tbody>
+      </table>
     </>
   );
 };
